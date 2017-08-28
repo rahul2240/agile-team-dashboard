802888d88885ab824bbeb0dd93429ab440965823
David Kang <dkang@suse.com>
2017-08-28 23:06:49 +0200

    Add CreateMeetings service

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -2,6 +2,14 @@ class Sprint < ApplicationRecord
   validates :start_date, :end_date, presence: true
 
   scope :current, (-> { find_by('start_date <= :today AND end_date >= :today', today: Time.zone.today) })
+
+  after_create :create_meetings
+
+  private
+
+  def create_meetings
+    CreateMeetings.run(self)
+  end
 end
 
 # == Schema Information
