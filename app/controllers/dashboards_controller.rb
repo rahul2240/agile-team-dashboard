4d5adb15b5df367e1fe1205babf187b4338ee374
David Kang <dkang@suse.com>
2017-08-29 19:17:56 +0200

    Add review as event_type in meeting

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,6 @@ class DashboardsController < ApplicationController
     @sprint = Sprint.current
     @absences = Absence.current
     @meetings = Meeting.today
-    @this_week = Meeting.today
     @pull_requests = github_pull_requests('openSUSE/open-build-service')
   end
 
