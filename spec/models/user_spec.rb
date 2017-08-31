526a79fd0c46a1e1242d3ebc0dff9e7b68dc3f98
Ana María Martínez Gómez <ammartinez@suse.de>
2017-08-31 15:50:19 +0200

    Test User#fullname
    Mob programmed by @mdeniz, @DavidKang and @Ana06.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe User, type: :model do
   %i(name).each do |attr|
     it { should validate_presence_of(attr) }
   end
+
+  let(:user) { create :user }
+
+  context '#fullname' do
+    it { expect(user.fullname).to eq("#{user.name} #{user.surname}") }
+  end
 end
