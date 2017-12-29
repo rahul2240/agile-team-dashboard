998d6cc5d848bed512e7507548b85f2f5aa4014f
AnkushMalik <ankushmalik631@gmail.com>
2017-12-29 16:24:26 +0530

    Add links to the sidebar for Static Pages
    Move the team info link inside a loop
    
    Closes #55

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
-  PAGES = [:build_solutions_team].freeze
+  PAGES = { build_solutions_team: 'Team Info' }.freeze
 
-  PAGES.each do |action_name|
+  PAGES.each_key do |action_name|
     define_method(action_name) do
       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true)
       @content = markdown.render(File.open(Rails.root.join('pages', "#{action_name}.md")).read)
